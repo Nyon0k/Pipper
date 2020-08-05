c511b62b94805c549b5da60143eadbed90c885a2
rafaelMartins1922 <rafael.martins@icomp.ufam.edu.br>
2020-08-05 19:53:27 -0300

    + Seed e Factory de Usuários

--- Pipper-Back/app/Providers/AppServiceProvider.php
+++ Pipper-Back/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Faker\Factory as FakerFactory;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(\Faker\Generator::class, function(){
+            return \Faker\Factory::create('pt-BR');
+        });
     }
 
     /**
