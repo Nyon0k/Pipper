da48ee67ca521cef91d4da97e29be613e1880701
cristianvilela0K@gmail.com <cristianvilela0K@gmail.com>
2020-08-06 03:30:08 -0300

    + Routes e attRequests

--- Pipper-Back/routes/api.php
+++ Pipper-Back/routes/api.php
@@ -21,20 +21,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Rotas de User
 Route::POST('createUser', 'UserController@createUser');
 Route::GET('showUser/{id}', 'UserController@showUser');
-Route::POST('listUsers', 'UserController@listUsers');
-Route::POST('updateUser/{id}', 'UserController@updateUser');
-Route::POST('deleteUser/{id}', 'UserController@deleteUser');
+Route::GET('listUsers', 'UserController@listUsers');
+Route::PUT('updateUser/{id}', 'UserController@updateUser');
+Route::DELETE('deleteUser/{id}', 'UserController@deleteUser');
+Route::POST('makePost/{user_id}/{post_id}', 'UserController@makePost');
+Route::POST('makeComment/{user_id}/{post_id}', 'UserController@makeComment');
 
 //Rotas de Post
 Route::POST('createPost', 'PostController@createPost');
 Route::GET('showPost/{id}', 'PostController@showPost');
-Route::POST('listPosts', 'PostController@listPost');
-Route::POST('updatePost/{id}', 'PostController@updatePost');
-Route::POST('deletePost/{id}', 'PostController@deletePost');
+Route::GET('listPosts', 'PostController@listPost');
+Route::PUT('updatePost/{id}', 'PostController@updatePost');
+Route::DELETE('deletePost/{id}', 'PostController@deletePost');
+//Route::GET('getUserId/{id}', 'PostController@getUserId');
+Route::PUT('attachComment/{post_id}/{comment_id}', 'PostController@attachComment');
 
 //Rotas de Comment
 Route::POST('createComment', 'CommentController@createComment');
 Route::GET('showComment/{id}', 'CommentController@showComment');
-Route::POST('listComments', 'CommentController@listComment');
-Route::POST('updateComment/{id}', 'CommentController@updateComment');
-Route::POST('deleteComment/{id}', 'CommentController@deleteComment');
+Route::GET('listComments', 'CommentController@listComment');
+Route::PUT('updateComment/{id}', 'CommentController@updateComment');
+Route::DELETE('deleteComment/{id}', 'CommentController@deleteComment');
+//Route::GET('getUserId/{id}', 'CommentController@getUserId');
+Route::GET('getPostId/{id}', 'CommentController@getPostId');
