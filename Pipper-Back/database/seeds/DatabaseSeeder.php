a31a270d840c5d1f73c20d4a4434d5e26afc5ba8
sunnyseth <8thseth@gmail.com>
2020-08-18 17:57:05 -0300

    #now seeds nickname wont start with @

--- Pipper-Back/database/seeds/DatabaseSeeder.php
+++ Pipper-Back/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(sUserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         //$this->call(TagTableSeeder::class);
         // $this->call(PostTableSeeder::class);
         //$this->call(CommentTableSeeder::class);
